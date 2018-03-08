34244a0e60595f99a42a41d26850d96ba770ccaf
SimonPusterhofer <pussid14@htlkaindorf.at>
2018-03-08 11:33:09 +0100

    Added Appstate for Maincharacter

---
+++ Tschainuk/src/playercharacter/PhysicalCharacterAppState.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playercharacter;
+
+import com.jme3.app.state.AbstractAppState;
+
+/**
+ *
+ * @author Simon Pusterhofer
+ */
+public class PhysicalCharacterAppState extends AbstractAppState
+{
+    
+}
