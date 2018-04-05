29c2e61fb093b48b3f9eee6308ff2d0bd53271d2
Thomas Eberl <ebethd14@htlkaindorf.at>
2018-04-05 11:18:13 +0200

    Added FP Cam

--- Tschainuk/src/playercharacter/PhysicalCharacterAppState.java
+++ Tschainuk/src/playercharacter/PhysicalCharacterAppState.java
@@ -6,13 +6,10 @@ import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.asset.AssetManager;
 import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
-import com.jme3.bullet.control.CharacterControl;
 import com.jme3.input.InputManager;
 import com.jme3.bullet.control.BetterCharacterControl;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
 import com.jme3.scene.Node;
 
 public class PhysicalCharacterAppState extends AbstractAppState {
@@ -22,10 +19,14 @@ public class PhysicalCharacterAppState extends AbstractAppState {
     private BulletAppState bulletAppState;
     private AppStateManager stateManager;
     private InputManager inputManager;
-    private PhysicalCharacterMovement movement;
+    private Camera flyCam;
+    private Node playerNode = new Node("playernode");
     private BetterCharacterControl playerControl;
-
-    public PhysicalCharacterAppState(BulletAppState bulletAppState) {
+    
+    private boolean moveLeft, moveRight, moveForward, moveBack;
+    
+    public PhysicalCharacterAppState(BulletAppState bulletAppState)
+    {
         this.bulletAppState = bulletAppState;
     }
 
@@ -34,37 +35,27 @@ public class PhysicalCharacterAppState extends AbstractAppState {
         // Load any model
         Node myCharacter = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
         //myCharacter.setLocalScale(1,0.55f,1);
-
+        
+        
         //create WrapperNode
-        Node playerNode = new Node("playernode");
         playerNode.setLocalTranslation(-7, 50, 0);
-
+        
         //attach nodes
         playerNode.attachChild(myCharacter);
-
+        
+        
         //create player control
         playerControl = new BetterCharacterControl(3f, 7f, 1f);
-        playerControl.setGravity(new Vector3f(0f, 1.5f, 0f));
+        playerControl.setGravity(new Vector3f(0f,1.5f,0f));
         playerNode.addControl(playerControl);
-
+    
+        
         //bulletAppState.setDebugEnabled(true);
+        
         //attach control and player to physicspace
         bulletAppState.getPhysicsSpace().add(playerNode);
-
+        
         rootNode.attachChild(playerNode);
-    }
-
-    private void setUpKeys() {
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-        inputManager.addListener(movement, "Left");
-        inputManager.addListener(movement, "Right");
-        inputManager.addListener(movement, "Up");
-        inputManager.addListener(movement, "Down");
-        inputManager.addListener(movement, "Jump");
     }
 
     @Override
@@ -75,16 +66,20 @@ public class PhysicalCharacterAppState extends AbstractAppState {
 
         this.stateManager = stateManager;
         this.inputManager = app.getInputManager();
-        movement = new PhysicalCharacterMovement(playerControl, app.getCamera());
+        this.flyCam = app.getCamera();
         
         initChar();
-        setUpKeys();
-
     }
 
     @Override
-    public void update(float tpf) {
+    public void update(float tpf) 
+    {
         super.update(tpf);
+        
+        Vector3f camDir = flyCam.getDirection();
+        Vector3f playerPos = playerNode.getWorldTranslation();
+        
+        playerControl.setViewDirection(camDir);
+        flyCam.setLocation(playerPos);
     }
-
 }
